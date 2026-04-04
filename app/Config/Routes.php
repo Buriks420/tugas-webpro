1cbc606d9aa5fdb8bb3f08ac5ed9ec2c4e5ac784
Buriks420 <akuncobax1a@gmail.com>
2026-04-04 14:21:05 +0700

    routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,26 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Auth::index');
+$routes->get('/auth', 'Auth::index');
+$routes->post('/auth/login', 'Auth::login');
+$routes->get('/auth/logout', 'Auth::logout');
+
+$routes->get('/dashboard', 'Dashboard::index');
+
+$routes->get('/kategori', 'Kategori::index');
+$routes->get('/kategori/create', 'Kategori::create');
+$routes->post('/kategori/store', 'Kategori::store');
+
+$routes->get('/alat', 'Alat::index');
+$routes->get('/alat/create', 'Alat::create');
+$routes->post('/alat/store', 'Alat::store');
+
+$routes->get('/penyewa', 'Penyewa::index');
+$routes->get('/penyewa/create', 'Penyewa::create');
+$routes->post('/penyewa/store', 'Penyewa::store');
+
+$routes->get('/transaksi', 'Transaksi::index');
+$routes->get('/transaksi/create', 'Transaksi::create');
+$routes->post('/transaksi/store', 'Transaksi::store');
+$routes->get('/transaksi/kembalikan/(:num)', 'Transaksi::kembalikan/$1');
