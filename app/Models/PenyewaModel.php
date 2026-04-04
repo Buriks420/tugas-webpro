fe9d59a847d6bfd866dd7fe8fc9a16dbef645aca
Buriks420 <akuncobax1a@gmail.com>
2026-04-04 14:08:13 +0700

    add PenyewaModel for managing tenant data

---
+++ app/Models/PenyewaModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class PenyewaModel extends Model
+{
+    protected $table = 'penyewa';
+    protected $primaryKey = 'id_penyewa';
+    protected $allowedFields = ['nama_penyewa', 'kontak', 'alamat'];
+}
