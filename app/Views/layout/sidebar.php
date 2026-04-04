e885aa19083d7c421d8e268ab35e56d9cca0b914
Buriks420 <akuncobax1a@gmail.com>
2026-04-04 14:19:26 +0700

    add layout files for header, footer, and sidebar with Bootstrap integration

---
+++ app/Views/layout/sidebar.php
@@ -0,0 +1,13 @@
+<div class="bg-dark text-white p-3" style="width: 250px; min-height: 100vh;">
+        <h4>Admin Panel</h4>
+        <hr>
+        <ul class="nav flex-column">
+            <li class="nav-item"><a href="/dashboard" class="nav-link text-white">Dashboard</a></li>
+            <li class="nav-item"><a href="/kategori" class="nav-link text-white">Kategori Alat</a></li>
+            <li class="nav-item"><a href="/alat" class="nav-link text-white">Data Alat</a></li>
+            <li class="nav-item"><a href="/penyewa" class="nav-link text-white">Data Penyewa</a></li>
+            <li class="nav-item"><a href="/transaksi" class="nav-link text-white">Transaksi Penyewaan</a></li>
+            <li class="nav-item mt-4"><a href="/auth/logout" class="nav-link text-danger">Logout</a></li>
+        </ul>
+    </div>
+    <div class="container-fluid p-4">
