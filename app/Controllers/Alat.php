15739bea6ab0b07075f531e4a288a631d01ff58c
Buriks420 <akuncobax1a@gmail.com>
2026-04-04 14:11:56 +0700

    add Alat controller for managing alat data with CRUD operations

---
+++ app/Controllers/Alat.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\AlatModel;
+use App\Models\KategoriModel;
+
+class Alat extends BaseController
+{
+    protected $alatModel;
+    protected $kategoriModel;
+
+    public function __construct()
+    {
+        $this->alatModel = new AlatModel();
+        $this->kategoriModel = new KategoriModel();
+    }
+
+    public function index()
+    {
+        $data = [
+            'title' => 'Data Alat',
+            'alat' => $this->alatModel->getAlatWithKategori()
+        ];
+
+        echo view('layout/header', $data);
+        echo view('layout/sidebar');
+        echo view('alat/index', $data);
+        echo view('layout/footer');
+    }
+
+    public function create()
+    {
+        $data = [
+            'title' => 'Tambah Alat',
+            'kategori' => $this->kategoriModel->findAll()
+        ];
+
+        echo view('layout/header', $data);
+        echo view('layout/sidebar');
+        echo view('alat/create', $data);
+        echo view('layout/footer');
+    }
+
+    public function store()
+    {
+        $fileFoto = $this->request->getFile('foto');
+        $namaFoto = $fileFoto->getRandomName();
+        $fileFoto->move('uploads', $namaFoto);
+
+        $this->alatModel->save([
+            'nama_alat' => $this->request->getVar('nama_alat'),
+            'id_kategori' => $this->request->getVar('id_kategori'),
+            'deskripsi' => $this->request->getVar('deskripsi'),
+            'foto' => $namaFoto
+        ]);
+
+        return redirect()->to('/alat');
+    }
+}
